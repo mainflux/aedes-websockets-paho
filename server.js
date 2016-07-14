5a6b19f1a7f88e55f14cd4ce1c7eb2135fd686a3
laszbalo <laszbalo@gmail.com>
2016-07-14 21:24:03 +0100

    Test with MQTT.js as a client

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ const wss = require('websocket-stream')
 const aedes = require('aedes')()
 
 const index = path.join(__dirname, 'index.html')
+const mqttjs = path.join(__dirname, 'mqtt.lib.js')
 
 const server = http.createServer((req, res)  => {
 
 	if(req.url === '/' || req.url === '/index.html') {
 		res.writeHead(200, {'Content-Type': 'text/html'})
 		fs.createReadStream(index).pipe(res)
+	} else if(req.url === '/mqtt.lib.js'){
+		res.writeHead(200, {'Content-Type': 'application/javascript'})
+		fs.createReadStream(mqttjs).pipe(res)
 	}
 	
 })
 
 const ws = wss.createServer({
-	server: server
+	server: server,
 }, aedes.handle)
 
 server.listen(9002, () => {
